ab67275050c3534ad1c6376934259b70a89f0000
T <archt723@gmail.com>
2025-08-29 11:43:36 +0900

    [Auto-sync] 2025-08-29 11:43:36

--- services/attendance/mobile/lib/main.dart
+++ services/attendance/mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/theme/neo_brutal_theme.dart';
