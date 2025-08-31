80039590087d2d3384937ff694865ce2f8c79994
T <archt723@gmail.com>
2025-09-01 01:17:58 +0900

    [Auto-sync] 2025-09-01 01:17:58

--- services/attendance/mobile/lib/presentation/pages/admin/qr_display_page.dart
+++ services/attendance/mobile/lib/presentation/pages/admin/qr_display_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/services/qr_service.dart';
 import '../../../core/theme/neo_brutal_theme.dart';
