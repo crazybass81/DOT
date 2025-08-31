d816322fcbc8740445be2b479d5104ee68798985
T <archt723@gmail.com>
2025-08-31 23:24:03 +0900

    [Auto-sync] 2025-08-31 23:24:03

--- services/attendance/mobile/lib/core/di/injection_container.dart
+++ services/attendance/mobile/lib/core/di/injection_container.dart
@@ -17,10 +17,13 @@ import '../services/attendance_service.dart';
 
 import '../../data/datasources/auth/auth_local_datasource.dart';
 import '../../data/datasources/auth/auth_remote_datasource.dart';
+import '../../data/datasources/auth/firebase_auth_remote_datasource.dart';
 import '../../data/datasources/attendance/attendance_local_datasource.dart';
 import '../../data/datasources/attendance/attendance_remote_datasource.dart';
 import '../../data/datasources/user/user_local_datasource.dart';
 import '../../data/datasources/user/user_remote_datasource.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../data/repositories/auth_repository_impl.dart';
 import '../../data/repositories/attendance_repository_impl.dart';
